01963b7a6868bd5f4a06ca85f414ef19275b4170
taha-syed-19 <tahas752@gmail.com>
2024-12-16 20:32:34 +0530

    Create script.groovy

---
+++ test/script.groovy
@@ -0,0 +1,14 @@
+def buildApp() {
+    echo 'building the application based on server you selected'
+}
+
+def testApp() {
+    echo 'Testing the App'
+}
+
+def deployApp() {
+    echo 'Deploying the App'
+    echo "Deploying version ${params.VERSION}"
+}
+
+return this
